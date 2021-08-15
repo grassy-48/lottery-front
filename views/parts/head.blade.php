70c5695e6b4e0e7d9d4e947d514435d300acda97
grassy-48 <shiba.se14@gmail.com>
2021-08-15 16:31:03 +0900

    add favicon

--- views/parts/head.blade.php
+++ views/parts/head.blade.php
@@ -4,6 +4,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>山月ペーパーラリー企画『1211通目のラブレター』 | @yield('title')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="https://ymtk.xyz/loveletter/wp-content/uploads/2020/01/cropped-rogo-32x32.png" sizes="32x32" />
+    <link rel="icon" href="https://ymtk.xyz/loveletter/wp-content/uploads/2020/01/cropped-rogo-192x192.png" sizes="192x192" />
+    <link rel="apple-touch-icon" href="https://ymtk.xyz/loveletter/wp-content/uploads/2020/01/cropped-rogo-180x180.png" />
     <link rel="stylesheet" href="https://jenil.github.io/bulmaswatch/minty/bulmaswatch.min.css">
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
